1217d2f960e2d1673d35d8f1b6a5e0aa50327962
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-03-27 03:37:03 -0500

    Initial Commit

---
+++ Earth/EarthApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthApp.swift
+//  Earth
+//
+//  Created by Andy Yu on 2023/3/27.
+//
+
+import SwiftUI
+
+@main
+struct EarthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
