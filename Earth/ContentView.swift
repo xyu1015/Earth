97a8850f183568a3569ad4d0ef0e3d5f9f7d0651
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-03-27 16:00:36 -0500

    地球大小设置
    非固定

--- Earth/ContentView.swift
+++ Earth/ContentView.swift
@@ -1,25 +1,56 @@
-//
-//  ContentView.swift
-//  Earth
-//
-//  Created by Andy Yu on 2023/3/27.
-//
-
 import SwiftUI
+import SceneKit
+import ARKit
 
-struct ContentView: View {
+struct ContentView : View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ARViewContainer().edgesIgnoringSafeArea(.all)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+struct ARViewContainer: UIViewRepresentable {
+    @State private var currentAngle: Float = 0
+    func makeUIView(context: Context) -> SCNView {
+        let sceneView = SCNView()
+        let scene = SCNScene(named: "Earth.usdz")!
+        let earthNode = scene.rootNode.childNodes.first!
+        earthNode.position = SCNVector3(0, 0, -300) // 移动到相机前方
+        
+        sceneView.contentMode = .scaleAspectFit
+        sceneView.scene = scene
+        sceneView.allowsCameraControl = true
+
+        sceneView.backgroundColor = UIColor.white
+        
+        return sceneView
+    }
+    
+    func updateUIView(_ uiView: SCNView, context: Context) {}
+    
+    typealias UIViewType = SCNView
+    
+    
+    func panGesture(_ recognizer: UIPanGestureRecognizer) {
+        // Get the translation of the gesture
+        let translation = recognizer.translation(in: recognizer.view)
+            
+        // Convert the translation to a rotation angle
+        let rotationAngle = Float(translation.x) * (-5)
+            
+        // Update the current rotation angle
+        currentAngle += rotationAngle
+            
+        // Reset the translation to zero to prevent cumulative rotation
+        recognizer.setTranslation(.zero, in: recognizer.view)
+    }
+
+    
+    
+}
+
+
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
     }
